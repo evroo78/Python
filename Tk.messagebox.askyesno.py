4ff464c35650c32c9de365b0b4bcd49137df9f19
Hanna Shevliakova <48261181+evroo78@users.noreply.github.com>
2022-02-07 21:53:29 +0200

    Update Tk.messagebox.askyesno.py

--- Tk.messagebox.askyesno.py
+++ Tk.messagebox.askyesno.py
@@ -4,6 +4,11 @@ def f():
   if (askyesno (title="Запит", message="Перенести дані?")):
     l['text'] = e.get()
     e.delete(0, END)
+#askokcancel - Ok / Cancel
+#askquestion - Yes / No
+#askretrycancel - Retry / Cancel
+#askyesnocancel - Yes / No / Cancel
+
 root = Tk()
 e = Entry()
 l = Label()
